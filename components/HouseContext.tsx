6716910a3e7ecfbfd8803109de809085f6b00885
IsmailDX <ismaildxx@gmail.com>
2023-10-22 17:50:20 +0400

    filtering now works yay!

--- components/HouseContext.tsx
+++ components/HouseContext.tsx
@@ -78,7 +78,63 @@ const HouseContextProvider = ({ children }: HouseContextProviderProps) => {
   }, []);
 
   const handleClick = () => {
-    console.log("clicked");
+    setLoading(true);
+
+    const isDefault = (str: string) => {
+      return str.split(" ").includes("(any)");
+    };
+
+    const minPrice = parseInt(price.split(" ")[0]);
+    const maxPrice = parseInt(price.split(" ")[2]);
+
+    const newHouses = housesData.filter((house) => {
+      const housePrice = parseInt(house.price);
+      if (
+        house.country === country &&
+        house.type === property &&
+        housePrice >= minPrice &&
+        housePrice <= maxPrice
+      ) {
+        return house;
+      }
+      if (isDefault(country) && isDefault(property) && isDefault(price)) {
+        return house;
+      }
+
+      if (!isDefault(country) && isDefault(property) && isDefault(price)) {
+        return house.country == country;
+      }
+
+      if (isDefault(country) && !isDefault(property) && isDefault(price)) {
+        return house.type == property;
+      }
+
+      if (isDefault(country) && isDefault(property) && !isDefault(price)) {
+        if (housePrice >= minPrice && housePrice <= maxPrice) {
+          return house;
+        }
+      }
+
+      if (!isDefault(country) && !isDefault(property) && isDefault(price)) {
+        return house.country === country && house.type === property;
+      }
+
+      if (!isDefault(country) && isDefault(property) && !isDefault(price)) {
+        if (housePrice >= minPrice && housePrice <= maxPrice) {
+          return house.country === country;
+        }
+      }
+
+      if (!isDefault(country) && !isDefault(property) && !isDefault(price)) {
+        if (housePrice >= minPrice && housePrice <= maxPrice) {
+          return house.type == property;
+        }
+      }
+    });
+    setTimeout(() => {
+      return newHouses.length < 1 ? setHouses([]) : setHouses(newHouses);
+      setLoading(false);
+    }, 1000);
   };
 
   return (
